fff5ae586ce89f76b060352af808044f01e6e1b5
Igor Alexandrov <igor.alexandrov@gmail.com>
2023-02-09 15:43:14 +0400

    Fixed Rubocop offenses

--- lib/category.rb
+++ lib/category.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 module Category
-  class Base;
+  class Base
     def self.name
-      self.to_s.split('::').last
+      to_s.split('::').last
     end
   end
 
   class Authentication < Base; end
+
   class Other < Base; end
 end
