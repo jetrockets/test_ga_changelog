40d9be62472c55fa77698d08ccef818f10314269
Igor Alexandrov <igor.alexandrov@gmail.com>
2023-02-10 10:14:49 +0400

    Added link to rodauth-rails

--- lib/ruby/rodauth.rb
+++ lib/ruby/rodauth.rb
@@ -8,4 +8,5 @@ class Rodauth < Curated::RubyGem
   pros 'One of the key benefits of using Rodauth is that it provides a consistent and secure implementation of these features, following best practices for security and user experience. This can save time and reduce the risk of security vulnerabilities, as developers do not have to reinvent the wheel and can instead rely on well-tested code.'
   pros 'Rodauth is highly customizable and extensible, allowing developers to easily modify or add new features as needed. This means that it can be adapted to meet the specific needs of different projects, while still providing a solid foundation for authentication and authorization.'
   pros 'Rodauth can be used with Rails or any other Rack application, making it a flexible and versatile solution for authentication and authorization.'
+  pros 'To use Rodauth with Rails, you should use `rodauth-rails` [https://github.com/janko/rodauth-rails](https://github.com/janko/rodauth-rails) gem, which provides a simple and straightforward way to integrate Rodauth into a Rails application.'
 end
